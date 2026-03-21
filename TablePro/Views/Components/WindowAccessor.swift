aa1cab5630998a8acd7b45b4e5292ae079a33e61
Ngo Quoc Dat <datlechin@gmail.com>
2026-03-21 18:54:59 +0700

    Revert "fix: prevent crash in WindowAccessor by using viewDidMoveToWindow"
    This reverts commit 9938a5a8d2accab351b643f0a9d58744590fefdf.

--- TablePro/Views/Components/WindowAccessor.swift
+++ TablePro/Views/Components/WindowAccessor.swift
@@ -3,8 +3,7 @@
 //  TablePro
 //
 //  Captures the hosting NSWindow from SwiftUI via an invisible NSView.
-//  Uses viewDidMoveToWindow to capture once, avoiding repeated async
-//  dispatches that can race with window deallocation.
+//  Avoids brittle title-matching or NSApp.keyWindow heuristics.
 //
 
 import SwiftUI
@@ -12,26 +11,9 @@ import SwiftUI
 struct WindowAccessor: NSViewRepresentable {
     @Binding var window: NSWindow?
 
-    func makeNSView(context: Context) -> WindowAccessorView {
-        let view = WindowAccessorView()
-        view.onWindowChange = { [self] newWindow in
-            self.window = newWindow
-        }
-        return view
-    }
+    func makeNSView(context: Context) -> NSView { NSView() }
 
-    func updateNSView(_ nsView: WindowAccessorView, context: Context) {
-        nsView.onWindowChange = { [self] newWindow in
-            self.window = newWindow
-        }
-    }
-}
-
-final class WindowAccessorView: NSView {
-    var onWindowChange: ((NSWindow?) -> Void)?
-
-    override func viewDidMoveToWindow() {
-        super.viewDidMoveToWindow()
-        onWindowChange?(window)
+    func updateNSView(_ nsView: NSView, context: Context) {
+        DispatchQueue.main.async { self.window = nsView.window }
     }
 }
